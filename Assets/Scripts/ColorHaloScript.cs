63920f6e783580b3d439fa234a00abb1ca5fb45b
Tin Huynh <tinn_1998@yahoo.com>
2014-12-25 16:07:20 -0800

    Added halo color transition
    Halo now slowly transition to the new color for added effect.
    Also has the side effect of making the player glow white at the
    beginning of the level (which may or may not be a good thing)

--- Assets/Scripts/ColorHaloScript.cs
+++ Assets/Scripts/ColorHaloScript.cs
@@ -6,6 +6,7 @@ public class ColorHaloScript : MonoBehaviour {
     public int spinRate; //Rate at which color halo spins
     private SpriteRenderer spriteRenderer;
     private Color currentColor;
+    public float colorTransSpeed;
 
     // Use this for initialization
     void Start() {
@@ -14,17 +15,29 @@ public class ColorHaloScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (halo1) {
-            //currentColor = Global.PlayerColor1;
-            currentColor = Global.PlayerColor1;
-        } else {
-            currentColor = Global.PlayerColor2;
+        if (halo1) { //Halo 1
+            if(Global.PlayerColor1 != Color.white){ //If color 1 has a color
+                currentColor = Global.PlayerColor1;
+            }
+            else{
+                currentColor.a = 0; //Make halo invisible
+            }
+        } 
+        else { //Halo 2
+            if(Global.PlayerColor2 != Color.white){ //If color 2 has a color
+                currentColor = Global.PlayerColor2;
+            }
+            else if(Global.PlayerColor1 != Color.white) { //If color 1 has a color
+                currentColor = Global.PlayerColor1;
+            }
+            else{
+                currentColor.a = 0;
+            }
         }
-        if (currentColor != Color.white) { //If the halo has a color
-            spriteRenderer.color = new Color(currentColor.r, currentColor.g, currentColor.b, 0.5f);
-        } else {
-            spriteRenderer.color = new Color(0, 0, 0, 0);
+        if(currentColor.a != 0){
+            currentColor.a = 0.5f; //Make halo transparent
         }
+        spriteRenderer.color = Color.Lerp(spriteRenderer.color, currentColor, colorTransSpeed * Time.deltaTime);
         transform.rotation = Quaternion.Euler(new Vector3(0,0, transform.rotation.eulerAngles.z + spinRate));
     }
 }
